26bf76771ffe1a36e3e876c825d8048d1a1b2401
Jonas18121 <jonasxtrm971@gmail.com>
2021-06-09 12:23:18 +0200

    test géoloc

--- js/HelloWorldSceneAR.js
+++ js/HelloWorldSceneAR.js
@@ -6,6 +6,8 @@ import {StyleSheet, PermissionsAndroid} from 'react-native';
 
 import Geolocation from 'react-native-geolocation-service';
 
+import projector from 'ecef-projector';
+
 import {
   ViroARScene,
   ViroText,
@@ -21,11 +23,21 @@ export default class HelloWorldSceneAR extends Component {
     this.state = {
       text : "Initializing AR...",
       text_2 : '',
+      geolocation : {
+        latitude : null,
+        longitude : null,
+        count: 0,
+        selisih: null,
+        xSel: null,
+        ySel: null,
+        zSel: null,
+        distance: null,
+      }
     };
 
     // bind 'this' to functions
     this._onInitialized = this._onInitialized.bind(this);
-    this._getLocation = this._getLocation.bind(this)
+    // this._getLocation = this._getLocation.bind(this)
     this._showPosition = this._showPosition.bind(this)
   }
 
@@ -58,7 +70,65 @@ export default class HelloWorldSceneAR extends Component {
     
   }
 
-  _getLocation() {
+  getCoordinate = () => {
+
+    const userxyz = projector.project(this.state.geolocation.latitude, this.state.geolocation.longitude, 0.0);
+    const objectxyz = projector.project(-6.260719, 106.781616, 0.0)
+
+    if (this.state.geolocation.count === 0) {
+        this.selisih(userxyz, objectxyz)
+    }
+    const distance = this.getDistance(this.state.geolocation.latitude, this.state.geolocation.longitude, -6.260719, 106.781616)
+    this.setState({
+        geolocation: {
+            distance: distance
+        }
+    })
+}
+
+selisih = (user, object) => {
+
+    let selisih = {
+        x: user[0] - object[0],
+        y: user[1] - object[1],
+        z: user[2] - object[2]
+    }
+    this.setState({
+        geolocation: {
+            xSel: selisih.x,
+            ySel: selisih.y,
+            zSel: selisih.z,
+            count: this.state.geolocation.count + 1
+        }
+    })
+}
+
+
+getDistance = (lon1, lat1, lon2, lat2) => {
+    if (typeof(Number.prototype.toRad) === "undefined") {
+        Number.prototype.toRad = function() {
+            return this * Math.PI / 180;
+        }
+    }
+
+    /* var R = 6371; // Radius of the earth in km
+
+    var dLat = (lat2-lat1).toRad();  // Javascript functions in radians
+
+    var dLon = (lon2-lon1).toRad(); // distance longitude
+
+    var a = Math.sin(dLat/2) * Math.sin(dLat/2) +
+            Math.cos(lat1.toRad()) * Math.cos(lat2.toRad()) *
+            Math.sin(dLon/2) * Math.sin(dLon/2);
+
+    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+
+    var d = R * c * 1000 // Distance in km
+
+    return d.toFixed(2) */
+}
+
+  _getLocation = () => {
     if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition(this._showPosition);
     } else {
@@ -69,17 +139,43 @@ export default class HelloWorldSceneAR extends Component {
   _showPosition(position) {
     // return "Latitude: " + position.coords.latitude
     // "<br>Longitude: " + position.coords.longitude;
-
     this.setState({
-      text_2 : "Latitude: " + position.coords.latitude
+      text_2 : "Latitude: " + position.coords.longitude,
+      geolocation : {
+        latitude : position.coords.latitude,
+        longitude : position.coords.longitude
+      }
     });
+
+    const userxyz = projector.project(this.state.geolocation.latitude, this.state.geolocation.longitude, 0.0);
+    const objectxyz = projector.project(-6.260719, 106.781616, 0.0)
+
+    if (this.state.geolocation.count === 0) {
+        this.selisih(userxyz, objectxyz)
+    }
+    /*const distance = this.getDistance(this.state.geolocation.latitude, this.state.geolocation.longitude, -6.260719, 106.781616)
+    this.setState({
+        geolocation: {
+            distance: distance
+        }
+    }) */
+
+    /* this.setState({
+      text_2 : "mon test: " + this.state.geolocation.latitude,
+    }); */
+
+    
   }
   
   render() {
+
+    const numx = this.state.geolocation.xSel;
+    const numy = this.state.geolocation.ySel;
+    const numz = this.state.geolocation.zSel;
     return (
       <ViroARScene onTrackingUpdated={this._onInitialized} >
-        <ViroText text={this.state.text} scale={[.5, .5, .5]} position={[0, 0, -1]} style={styles.helloWorldTextStyle} />
-        <ViroText text={this.state.text_2} scale={[.5, .5, .5]} position={[-1, 0, -1]} style={styles.helloWorldTextStyle} />
+        {/* <ViroText text={this.state.text} scale={[.5, .5, .5]} position={[0, 0, -1]} style={styles.helloWorldTextStyle} /> */}
+        <ViroText text={this.state.text_2} scale={[.5, .5, .5]} position={[this.state.geolocation.latitude, 0, this.state.geolocation.longitude]} style={styles.helloWorldTextStyle} />
       </ViroARScene>
     );
   }
